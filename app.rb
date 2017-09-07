f167eb3f46045a34cb3c3ae698853c255a78f5e0
Tom Groombridge <groombridgetom@gmail.com>
2017-09-07 18:55:13 +0100

    added response for the endpoint

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'alexa_rubykit'
+require 'json'
+
+before do
+  content_type('application/json')
+end
 
 get '/' do
-  "Zopa Alexa aka Echo!!!"
+  response = AlexaRubykit::Response.new
+  response.add_speech('Ruby is running ready!')
+  puts response.build_response
+  response.build_response
 end
 
 
