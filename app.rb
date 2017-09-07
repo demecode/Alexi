9bed6e9912b927cce0755d8eb994de51a8d8e3b7
Tom Groombridge <groombridgetom@gmail.com>
2017-09-07 20:10:41 +0100

    basic app format

--- app.rb
+++ app.rb
@@ -1,17 +1,66 @@
+# Alexa RubyEngine
+# This Engine receives and responds to Amazon Echo's (Alexa) JSON requests.
 require 'sinatra'
-require 'sinatra/reloader'
-require 'alexa_rubykit'
 require 'json'
+require 'bundler/setup'
+require 'alexa_rubykit'
 
+# We must return application/json as our content type.
 before do
   content_type('application/json')
 end
 
-get '/' do
+#enable :sessions
+post '/' do
+  # puts "start request #{request.body.to_h}"
+  # # Check that it's a valid Alexa request
+  # request_json = JSON.parse(request.body.read.to_s)
+
+  # puts "request_json variable is equal to #{request_json}"
+  # # Creates a new Request object with the request parameter.
+  # request = AlexaRubykit.build_request(request_json)
+
+  # # We can capture Session details inside of request.
+  # # See session object for more information.
+  # session = request.session
+  # p session.new?
+  # p session.has_attributes?
+  # p session.session_id
+  # p session.user_defined?
+
+  # # We need a response object to respond to the Alexa.
+
+  # # We can manipulate the request object.
+  # #
+  # #p "#{request.to_s}"
+  # #p "#{request.request_id}"
+
+  # # Response
+  # # If it's a launch request
+  # if (request.type == 'LAUNCH_REQUEST')
+  #   # Process your Launch Request
+  #   # Call your methods for your application here that process your Launch Request.
+  #   response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Ruby Running Ready!' } )
+  # end
+
+  # if (request.type == 'INTENT_REQUEST')
+  #   # Process your Intent Request
+  #   p "#{request.slots}"
+  #   p "#{request.name}"
+  #   response.add_speech("I received an intent named #{request.name}?")
+  #   response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{request.name}" } )
+  # end
+
+  # if (request.type =='SESSION_ENDED_REQUEST')
+  #   # Wrap up whatever we need to do.
+  #   p "#{request.type}"
+  #   p "#{request.reason}"
+  #   halt 200
+  # end
+
+  # # Return response
+  puts 'hello world!!!!!!!!!!!!!!'
   response = AlexaRubykit::Response.new
-  response.add_speech('Ruby is running ready!')
-  puts response.build_response
+  response.add_speech('Zopa Demo is now live!')
   response.build_response
 end
-
-
